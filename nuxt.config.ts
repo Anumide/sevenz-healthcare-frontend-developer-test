14358b851bab23c6f882a495e8b9f2003738d8c0
Alaba Samuel Ayomide <alabasamuelayomide3@gmail.com>
2023-06-20 16:47:51 +0100

    changed ssr to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
+	target: 'static',
+	head: {
+		title: 'Traq',
+		htmlAttrs: { lang: 'en' },
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ name: 'format-detection', content: 'telephone=no' }
+		]
+	},
   modules: ['@nuxtjs/apollo', '@bootstrap-vue-next/nuxt'],
   css: ['bootstrap/dist/css/bootstrap.min.css', '/src/assets/css/main.css'],
   alias: {
